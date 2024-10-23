4e0d32d9dec639346c9d6b09f31797bfcadda458
Kholifahdina <kholifahdinaa@gmail.com>
2024-10-23 19:06:22 +0700

    TP_06_PBB

---
+++ 06_Interaksi_Pengguna/TP/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  // Daftar halaman yang akan ditampilkan saat menu BottomNavigationBar dipilih
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Ini Halaman Beranda', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Ini Halaman Wisata', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Ini Halaman Profile', style: TextStyle(fontSize: 24))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan BottomNavigationBar'),
+          backgroundColor: Colors.orange,
+        ),
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Beranda',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Wisata',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.orange,
+          unselectedItemColor: Colors.grey,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
